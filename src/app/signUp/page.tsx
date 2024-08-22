13c9d28b32bf93a22394968411f82c249fea6b5f
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-08-22 17:14:08 +0900

    feat: add layout for SignIn and SignUp pages

---
+++ src/app/signUp/page.tsx
@@ -0,0 +1,68 @@
+import { FC } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import style from './page.module.css';
+import InputAdornment from '@mui/material/InputAdornment';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import LockIcon from '@mui/icons-material/Lock';
+
+const SignUp: FC = () => {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        backgroundColor: '#f5f5f5',
+        padding: 5,
+        borderRadius: 2,
+        marginTop: 40,
+      }}
+    >
+      <h1 className={style.header}>Sign Up</h1>
+      <form>
+        <Grid container spacing={2} direction="column">
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              type="email"
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailRoundedIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Password"
+              variant="outlined"
+              type="password"
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button fullWidth variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default SignUp;
