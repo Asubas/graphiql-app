84b21d96d21d3ce188356525668b3ca041bba8b0
Леха <znaxor74@mail.ru>
2024-09-06 09:59:33 +0300

    fix: update test for private btn

--- src/__tests__/components/PrivateBtn.test.tsx
+++ src/__tests__/components/PrivateBtn.test.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import PrivateBtn from '@/src/components/Buttons/PrivateBtn/PrivateBtn';
+import { useRouter } from 'next/navigation';
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+}));
 
 describe('PrivateBtn Component', () => {
   it('renders the button with the correct label', () => {
     const label = 'Private Area';
-    render(<PrivateBtn label={label} className="custom-class" />);
+    const mockRouter = {
+      push: jest.fn(),
+    };
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    render(<PrivateBtn label={label} className="custom-class" path="/private" />);
 
     const button = screen.getByRole('button', { name: label });
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent(label);
     expect(button).toHaveClass('custom-class');
+    button.click();
+    expect(mockRouter.push).toHaveBeenCalledWith('/private');
   });
 });
