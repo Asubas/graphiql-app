495be7218cf96f92ceb270ad4b221a3ff2378b06
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-09-06 15:51:29 +0900

    fix: fix redirect errors and expired token handle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,18 @@ import styles from './page.module.scss';
 import About from '../components/About/About';
 import Welcome from '../components/Welcome/Welcome';
 import { useAuth } from '../hooks/useAuthRedirect';
+import Loader from '../components/Loader/Loader';
 
 export default function Home() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <main className={styles.main}>
+        <Loader />
+      </main>
+    );
+  }
 
   return (
     <main className={styles.main}>
