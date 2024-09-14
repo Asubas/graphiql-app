481af6f4d3f3e30f45413ef288c7c115d0ac6302
Anton <lipan48@gmail.com>
2024-09-14 17:56:03 +0300

    DTR-7/fix: fix urlUtils tests

--- src/__tests__/utils/urlUtils.test.ts
+++ src/__tests__/utils/urlUtils.test.ts
@@ -1,78 +1,110 @@
-// import { encodeBase64, decodeBase64, encodeUrl, decodeUrl } from '@/src/utils/urlUtils';
 import { encodeBase64, encodeUrl } from '@/src/utils/urlUtils';
 
-describe('URL Utils', () => {
-  describe('encodeBase64', () => {
-    it('should encode a string to base64 correctly', () => {
-      const input = 'Hello, World!';
-      const encoded = encodeBase64(input);
-      expect(encoded).toBe('SGVsbG8sIFdvcmxkIQ==');
-    });
+describe('encodeBase64', () => {
+  it('should correctly encode a simple string to Base64', () => {
+    const data = 'Hello, World!';
+    const encodedData = encodeBase64(data);
+    expect(encodedData).toBe('SGVsbG8sIFdvcmxkIQ==');
   });
 
-  // describe('decodeBase64', () => {
-  //   it('should decode a base64 string correctly', () => {
-  //     const encoded = 'SGVsbG8sIFdvcmxkIQ==';
-  //     const decoded = decodeBase64(encoded);
-  //     expect(decoded).toBe('Hello, World!');
-  //   });
-
-  //   it('should return an empty string if decoding fails', () => {
-  //     const invalidEncoded = 'Invalid base64 string';
-  //     const decoded = decodeBase64(invalidEncoded);
-  //     expect(decoded).toBe('');
-  //   });
-  // });
-
-  describe('encodeUrl', () => {
-    it('should encode URL parameters correctly', () => {
-      const params = {
-        method: 'GET',
-        endpointUrl: 'https://api.example.com/data',
-        body: '{"key":"value"}',
-        headers: [{ key: 'Authorization', value: 'Bearer token' }],
-        queries: [{ key: 'page', value: '1' }],
-      };
-
-      const encodedUrl = encodeUrl(params);
-
-      expect(encodedUrl).toContain('/rest/GET/');
-      expect(encodedUrl).toContain(encodeBase64(params.endpointUrl));
-      expect(encodedUrl).toContain(encodeBase64(params.body!));
-      expect(encodedUrl).toContain(encodeBase64(JSON.stringify(params.headers)));
-      expect(encodedUrl).toContain(encodeBase64(JSON.stringify(params.queries)));
-    });
+  it('should correctly encode special characters', () => {
+    const data = 'Test@123!';
+    const encodedData = encodeBase64(data);
+    expect(encodedData).toBe('VGVzdEAxMjMh');
   });
 
-  // describe('decodeUrl', () => {
-  //   it('should decode URL parameters correctly', () => {
-  //     const params = [
-  //       'GET',
-  //       encodeBase64('https://api.example.com/data'),
-  //       encodeBase64('{"key":"value"}'),
-  //       encodeBase64(JSON.stringify([{ key: 'Authorization', value: 'Bearer token' }])),
-  //       encodeBase64(JSON.stringify([{ key: 'page', value: '1' }])),
-  //     ];
+  it('should correctly encode an empty string', () => {
+    const data = '';
+    const encodedData = encodeBase64(data);
+    expect(encodedData).toBe('');
+  });
+});
 
-  //     const decoded = decodeUrl(params);
+describe('encodeUrl', () => {
+  it('should correctly encode URL components', () => {
+    const params = {
+      method: 'GET',
+      endpointUrl: 'https://api.example.com/data',
+      body: '{"key": "value"}',
+      headers: [{ key: 'Authorization', value: 'Bearer token' }],
+      queries: [{ key: 'search', value: 'query' }],
+      variables: [{ key: 'var1', value: 'value1' }],
+    };
 
-  //     expect(decoded.method).toBe('GET');
-  //     expect(decoded.url).toBe('https://api.example.com/data');
-  //     expect(decoded.body).toBe('{"key":"value"}');
-  //     expect(decoded.headers).toEqual([{ key: 'Authorization', value: 'Bearer token' }]);
-  //     expect(decoded.queries).toEqual([{ key: 'page', value: '1' }]);
-  //   });
+    const encodedString = encodeUrl(params);
 
-  //   it('should handle decoding errors and return default values', () => {
-  //     const params = ['GET', 'invalidBase64', '', 'invalidBase64Headers', 'invalidBase64Queries'];
+    const expectedEncodedEndpointUrl = encodeBase64('https://api.example.com/data');
+    const expectedEncodedBody = encodeBase64('{"key": "value"}');
+    const expectedEncodedHeaders = encodeBase64(
+      JSON.stringify([{ key: 'Authorization', value: 'Bearer token' }]),
+    );
+    const expectedEncodedQueries = encodeBase64(
+      JSON.stringify([{ key: 'search', value: 'query' }]),
+    );
+    const expectedEncodedVariables = encodeBase64(
+      JSON.stringify([{ key: 'var1', value: 'value1' }]),
+    );
 
-  //     const decoded = decodeUrl(params);
+    expect(encodedString).toContain(expectedEncodedEndpointUrl);
+    expect(encodedString).toContain(expectedEncodedBody);
+    expect(encodedString).toContain(expectedEncodedHeaders);
+    expect(encodedString).toContain(expectedEncodedQueries);
+    expect(encodedString).toContain(expectedEncodedVariables);
+  });
 
-  //     expect(decoded.method).toBe('GET');
-  //     expect(decoded.url).toBe('');
-  //     expect(decoded.body).toBe('');
-  //     expect(decoded.headers).toEqual([]);
-  //     expect(decoded.queries).toEqual([]);
-  //   });
-  // });
+  it('should handle empty optional fields correctly', () => {
+    const params = {
+      method: 'POST',
+      endpointUrl: 'https://api.example.com/data',
+    };
+
+    const encodedString = encodeUrl(params);
+
+    const expectedEncodedEndpointUrl = encodeBase64('https://api.example.com/data');
+    const expectedEncodedBody = encodeBase64('{}');
+    const expectedEncodedHeaders = encodeBase64('{}');
+    const expectedEncodedQueries = encodeBase64('{}');
+    const expectedEncodedVariables = encodeBase64('{}');
+
+    expect(encodedString).toContain(expectedEncodedEndpointUrl);
+    expect(encodedString).toContain(expectedEncodedBody);
+    expect(encodedString).toContain(expectedEncodedHeaders);
+    expect(encodedString).toContain(expectedEncodedQueries);
+    expect(encodedString).toContain(expectedEncodedVariables);
+  });
+
+  it('should only include valid headers, queries, and variables in the encoded URL', () => {
+    const params = {
+      method: 'GET',
+      endpointUrl: 'https://api.example.com/data',
+      headers: [
+        { key: 'Authorization', value: 'Bearer token' },
+        { key: '', value: '' },
+      ],
+      queries: [
+        { key: 'search', value: 'query' },
+        { key: '', value: '' },
+      ],
+      variables: [
+        { key: 'var1', value: 'value1' },
+        { key: '', value: '' },
+      ],
+    };
+
+    const encodedString = encodeUrl(params);
+
+    const expectedEncodedHeaders = encodeBase64(
+      JSON.stringify([{ key: 'Authorization', value: 'Bearer token' }]),
+    );
+    const expectedEncodedQueries = encodeBase64(
+      JSON.stringify([{ key: 'search', value: 'query' }]),
+    );
+    const expectedEncodedVariables = encodeBase64(
+      JSON.stringify([{ key: 'var1', value: 'value1' }]),
+    );
+
+    expect(encodedString).toContain(expectedEncodedHeaders);
+    expect(encodedString).toContain(expectedEncodedQueries);
+    expect(encodedString).toContain(expectedEncodedVariables);
+  });
 });
